94bbe81831cd5bda0e00e9ddaae787384ba9de55
unknown <tranthithanhtrang1708@gmail.com>
2022-06-12 12:37:57 +0700

    update UseState tang gia tri

--- src/components/TestUseState.jsx
+++ src/components/TestUseState.jsx
@@ -2,13 +2,13 @@ import { useState } from "react"
 import React from 'react'
 
 export const TestUseState = () => {
-    const [state, setState] =useState('helu chú Tin')
+    const [state, setState] =useState(0)
     // muon xem gia tri sau khi setState thi de log o day( chắc là do render từ trên xuống dưới)
     // vì để đso nó hiện cái state trước rồi sau khi click thì nó hiện ra sétstate
     console.log(state);
     
     const btnsetState =() =>{
-        setState('bụng mỡ :)))');
+        setState(state +1);
         // còn để dưới đây thì sau khi click nó hiện state
         // console.log(state);
 }
