9d3dc374660121dddecc0c262eaa77d15c5059e8
unknown <tranthithanhtrang1708@gmail.com>
2022-06-12 12:14:31 +0700

    learn useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import TestMap from './components/TestMap';
+import { TestUseState } from './components/TestUseState';
 
 function App() {
   return (
     <div className="App">
-     <TestMap/>
+     {/* <TestMap/> */}
+     <TestUseState/>
     </div>
   );
 }
---
+++ src/components/TestUseState.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+import React from 'react'
+
+export const TestUseState = () => {
+    const [state, setState] =useState('helu chú Tin')
+    // muon xem gia tri sau khi setState thi de log o day( chắc là do render từ trên xuống dưới)
+    // vì để đso nó hiện cái state trước rồi sau khi click thì nó hiện ra sétstate
+    console.log(state);
+    
+    const btnsetState =() =>{
+        setState('bụng mỡ :)))');
+        // còn để dưới đây thì sau khi click nó hiện state
+        // console.log(state);
+}
+
+
+  return (
+    <div>
+        {/* hien ra so 0 */}
+        <p>{state}</p>
+        <button onClick ={btnsetState}>Click me</button>
+
+    </div>
+  )
+}
